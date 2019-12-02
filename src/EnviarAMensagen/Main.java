394310276cb720de9f48ce205ac375c553ecf868
caixet41 <caixet41@gmail.com>
2019-12-02 16:23:40 -0300

    versao concluida trabalho pronto

--- src/EnviarAMensagen/Main.java
+++ src/EnviarAMensagen/Main.java
@@ -1,6 +1,8 @@
 
 package EnviarAMensagen;
 
+import SalvaConversa.Cloud;
+import SalvaConversa.ProcessaConversa;
 import VerificaAndroid.Celular;
 import VerificaAndroid.Desatualizado;
 
@@ -10,6 +12,7 @@ public class Main {
       public static void main(String[] args) {
         Celular celular = new Celular();
         celular.AtualizarVersao(new Desatualizado());
+        ProcessaConversa processa = new Cloud();
         
                 
         Pessoa p = new Pessoa();
@@ -23,6 +26,8 @@ public class Main {
         p.SetEnvia(porSms);
         System.out.println("");
         p.envia();
+        System.out.println("");
+        processa.constroi();
     
     
     }
